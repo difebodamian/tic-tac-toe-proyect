a3cbf7c2a9b651989f3622f815767ce0466ee1cd
DamianDiFebo <97866841+difebodamian@users.noreply.github.com>
2023-02-01 19:11:31 -0300

    Delete first-vite-proyect directory

--- first-vite-proyect/src/CardTitle.jsx
+++
@@ -1,9 +0,0 @@
-import './App.css'
-
-export function CardTitle({text}) {
-  return(
-    <div className="tw-cardTitle">
-      <strong className="tw-cardTitle-text">{text}</strong>
-    </div>
-  )
-}
